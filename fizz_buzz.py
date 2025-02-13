6e6319e841608cbae77d28f9e0e35a969eff1f40
christian <c_vetter@outlook.de>
2025-02-13 17:54:54 +0100

    feat: Add case 9 -> Fizz

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -9,11 +9,14 @@ class TestFizzBuzzShould:
     def test_return_fizz(self) -> None:
         assert fizz_buzz(3) == "Fizz"
         assert fizz_buzz(6) == "Fizz"
+        assert fizz_buzz(9) == "Fizz"
 
 
 def fizz_buzz(number: int) -> str:
     if number == 3:
         return "Fizz"
     if number == 6:
+        return "Fizz"
+    if number == 9:
         return "Fizz"
     return str(number)
