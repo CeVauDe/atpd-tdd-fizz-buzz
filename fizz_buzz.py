cb4b2b8069f0baf05358af15e10b48b3ed043198
christian <c_vetter@outlook.de>
2025-02-13 17:45:25 +0100

    feat: add case 2

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -4,7 +4,8 @@ class TestFizzBuzzShould:
     def test_return_input(self):
 
         assert fizz_buzz(1) == "1"
+        assert fizz_buzz(2) == "2"
 
 
 def fizz_buzz(number: int) -> str:
-    return "1"
+    return "1" if number == 1 else "2"
