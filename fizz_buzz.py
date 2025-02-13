cdbaa2ea1a1c5d83123f3af419f589702a67f999
christian <c_vetter@outlook.de>
2025-02-13 17:53:00 +0100

    feat: Add case 3 -> Fizz

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -4,9 +4,13 @@ import pytest
 class TestFizzBuzzShould:
     @pytest.mark.parametrize("number,expectation",[(1, "1"), (2, "2"), (4, "4")])
     def test_return_input(self, number: int, expectation: str) -> None:
-
         assert fizz_buzz(number) == expectation
+
+    def test_return_fizz(self) -> None:
+        assert fizz_buzz(3) == "Fizz"
 
 
 def fizz_buzz(number: int) -> str:
+    if number == 3:
+        return "Fizz"
     return str(number)
