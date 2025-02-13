48d0409a128ab02bc28671b26aa3b0b5066c0076
christian <c_vetter@outlook.de>
2025-02-13 17:48:27 +0100

    refactor: remove duplications

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -9,8 +9,4 @@ class TestFizzBuzzShould:
 
 
 def fizz_buzz(number: int) -> str:
-    if number == 1:
-        return "1"
-    if number == 2:
-        return "2"
-    return "4"
+    return str(number)
