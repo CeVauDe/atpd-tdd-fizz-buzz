47b285f72772728a40dbd9a65d0c51dc289a1990
christian <c_vetter@outlook.de>
2025-02-13 17:50:53 +0100

    test: parametrize test

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,11 +1,11 @@
+import pytest
 
 
 class TestFizzBuzzShould:
-    def test_return_input(self):
+    @pytest.mark.parametrize("number,expectation",[(1, "1"), (2, "2"), (4, "4")])
+    def test_return_input(self, number: int, expectation: str) -> None:
 
-        assert fizz_buzz(1) == "1"
-        assert fizz_buzz(2) == "2"
-        assert fizz_buzz(4) == "4"
+        assert fizz_buzz(number) == expectation
 
 
 def fizz_buzz(number: int) -> str:
