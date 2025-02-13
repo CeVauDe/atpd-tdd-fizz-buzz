c3599e80cfb7a7865e24a944c25b52480381054c
christian <c_vetter@outlook.de>
2025-02-13 17:59:09 +0100

    feat: Add case 5 -> Buzz

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -10,8 +10,12 @@ class TestFizzBuzzShould:
     def test_return_fizz(self, number: int) -> None:
         assert fizz_buzz(number) == "Fizz"
 
+    def test_return_buzz(self) -> None:
+        assert fizz_buzz(5) == "Buzz"
 
 def fizz_buzz(number: int) -> str:
     if number % 3 == 0:
         return "Fizz"
+    if number == 5:
+        return "Buzz"
     return str(number)
